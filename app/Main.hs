72946b649bdb1ff1fedd557368d38b9510d5475e
collinstmatthew <collins.t.matthew@protonmail.com>
2021-01-10 11:10:02 +0000

    Ram type and doubling the number of ram addresses

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}          -- for forall
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
 import Prelude hiding (Bool(..),not,(&&),(||))
 
+import qualified Control.Category as Cat
+
+
 import Control.Arrow
 import Control.Monad
-
-import qualified Control.Category as Cat
 import Data.List
-
 import Data.Maybe
+import Data.Functor.Compose
 
+import Control.Lens.Tuple
+import Control.Lens.Setter
+import Control.Lens.Operators
 
 data Bit = High | Low deriving Eq
 
@@ -20,14 +26,38 @@ instance Show Bit where
     show High = "1"
     show Low  = "0"
 
-
 type Byte = (Bit,Bit,Bit,Bit,Bit,Bit,Bit,Bit)
 
+-- Types for constructing components
 
-type CarryIn = Bit
+type CarryIn  = Bit
 type CarryOut = Bit
 
+-- # TODO change this to a list
+type Address = [Bit]
+-- # TODO make the data out into a list as well
+type Data    = Bit
+type Write   = Bit
+
 newtype Circuit a b = Circuit { unCircuit :: a -> (Circuit a b, b) }
+
+-- also keep track of the size of the ram a.k.a the legnth of the adresses
+data Ram = Ram { unRam :: (Circuit (Address,Write,Data) Data),
+                    -- (log_2 number of addresses,log_2 size of databus)
+                 size :: (Int,Int)}
+
+-- This doubles the number of addresses that are held in the ram
+doubleRamAddress :: Ram -> Ram
+doubleRamAddress ram = Ram circuit $ size ram & _1 +~ 1 where
+    circuit = proc ((aNew:address),write,dat) -> do
+        -- a new is the new adress input
+        -- check the size of the list is correct by throwing an error if not
+        (do1,do0) <- decoder1to2  -< (aNew,dat)
+        d1        <- unRam ram    -< (address,write,do1)
+        d0        <- unRam ram    -< (address,write,do0)
+        dataOut   <- selector2to1 -< (aNew,(d0,d1))
+
+        returnA -< dataOut
 
 instance Cat.Category Circuit where
     id = Circuit $ \a -> (Cat.id, a)
@@ -48,6 +78,10 @@ instance ArrowLoop Circuit where
     loop (Circuit cir) = Circuit $ \b ->
         let (cir', (c,d)) = cir (b,d)
         in  (loop cir', c)
+
+--combine :: Circuit Ram a b -> Circuit (Ram a b) -> Circuit (Ram a b)
+--combine x y = a
+
 
 runCircuit :: Circuit a b -> [a] -> [b]
 runCircuit _ [] = []
@@ -86,14 +120,17 @@ halfAdderA = Circuit $ \(a,b) -> (halfAdderA, (a !|| b, a && b))
 
 fullAdderA :: Circuit (Bit,Bit,Bit) (Bit,Bit)
 fullAdderA = proc (carryIn,a,b) -> do
-    (s1,co1) <- halfAdderA -< (a,b)
-    (s2,co2) <- halfAdderA -< (carryIn,s1)
+
+    (s1,co1) <- halfAdderA         -< (a,b)
+    (s2,co2) <- halfAdderA         -< (carryIn,s1)
     carryOut <- arr (uncurry (||)) -< (co1,co2)
+
     wire -< (s2,carryOut)
 
 -- maybe can make this better by mapping over the tuple somehow
 adder8BitA :: Circuit (CarryIn, Byte, Byte) (CarryOut, Byte)
 adder8BitA = proc (carryIn,(a0,a1,a2,a3,a4,a5,a6,a7),(b0,b1,b2,b3,b4,b5,b6,b7)) -> do
+
     (s0,co0) <- fullAdderA -< (carryIn, a0, b0)
     (s1,co1) <- fullAdderA -< (co0, a1, b1)
     (s2,co2) <- fullAdderA -< (co1, a2, b2)
@@ -102,7 +139,8 @@ adder8BitA = proc (carryIn,(a0,a1,a2,a3,a4,a5,a6,a7),(b0,b1,b2,b3,b4,b5,b6,b7)) 
     (s5,co5) <- fullAdderA -< (co4, a2, b2)
     (s6,co6) <- fullAdderA -< (co5, a2, b2)
     (s7,co7) <- fullAdderA -< (co6, a2, b2)
-    wire   -< (co7,(s0,s1,s2,s3,s4,s5,s6,s7))
+
+    wire -< (co7,(s0,s1,s2,s3,s4,s5,s6,s7))
 
 wire :: Circuit a a
 wire = Cat.id
@@ -124,8 +162,10 @@ orGate = parallelInverters >>> nandGate
 
 orGate' :: Circuit (Bit,Bit) Bit
 orGate' = proc (a,b) -> do
+
     m1 <- nandGate -< (a,a)
     m2 <- nandGate -< (b,b)
+
     nandGate -< (m1,m2)
 
 delay :: a -> Circuit a a
@@ -148,6 +188,7 @@ latch last = Circuit $ f where
 
 latch8Bit :: Circuit (Bit, Byte) Byte
 latch8Bit = proc (write,(d7,d6,d5,d4,d3,d2,d1,d0)) -> do
+
     q0 <- latch Low -< (write,d0)
     q1 <- latch Low -< (write,d1)
     q2 <- latch Low -< (write,d2)
@@ -156,6 +197,7 @@ latch8Bit = proc (write,(d7,d6,d5,d4,d3,d2,d1,d0)) -> do
     q5 <- latch Low -< (write,d5)
     q6 <- latch Low -< (write,d6)
     q7 <- latch Low -< (write,d7)
+
     returnA -< (q7,q6,q5,q4,q3,q2,q1,q0)
 
 edgeDFlipFlop :: (Bit,Bit) -> Circuit Bit Bit
@@ -165,6 +207,7 @@ edgeDFlipFlop (lastClk,state) = Circuit $ f where
 
 rippleCounter8Bit :: Circuit Bit Byte
 rippleCounter8Bit = proc inp -> do
+
     q1 <- edgeDFlipFlop (Low,Low) -< inp
     q2 <- edgeDFlipFlop (Low,Low) -< q1
     q3 <- edgeDFlipFlop (Low,Low) -< q2
@@ -172,6 +215,7 @@ rippleCounter8Bit = proc inp -> do
     q5 <- edgeDFlipFlop (Low,Low) -< q4
     q6 <- edgeDFlipFlop (Low,Low) -< q5
     q7 <- edgeDFlipFlop (Low,Low) -< q6
+
     returnA -< (q7,q6,q5,q4,q3,q2,q1,inp)
 
 -- Is it possible to just define Selector 2 1
@@ -191,8 +235,9 @@ selector8to1 = arr $ f where
     f ((High,Low ,Low) ,(_,_,_,d4,_,_,_,_)) = d4
     f ((High,Low ,High),(_,_,d5,_,_,_,_,_)) = d5
     f ((High,High,Low) ,(_,d6,_,_,_,_,_,_)) = d6
-    f ((High,High,High),(d7,_,_,_,_,_,_,_))  = d7
+    f ((High,High,High),(d7,_,_,_,_,_,_,_)) = d7
 
+--(select, data)-> (d1,d0)
 decoder1to2 :: Circuit (Bit,Bit)(Bit,Bit)
 decoder1to2 = arr $ f where
     f (Low,dat)  = (Low,dat)
@@ -213,7 +258,9 @@ decoder3to8 = arr $ f where
 -- ((a2,a1,a0),write,dataIn) dataOut
 ram8t1 :: Circuit ((Bit,Bit,Bit),Bit,Bit) Bit
 ram8t1 = proc ((a2,a1,a0),write,dat) -> do
+
     (o7,o6,o5,o4,o3,o2,o1,o0) <- decoder3to8  -< ((a2,a1,a0),write)
+
     d0 <- latch Low -< (o0,dat)
     d1 <- latch Low -< (o1,dat)
     d2 <- latch Low -< (o2,dat)
@@ -224,17 +271,35 @@ ram8t1 = proc ((a2,a1,a0),write,dat) -> do
     d7 <- latch Low -< (o7,dat)
 
     dataOut <-  selector8to1 -< ((a2,a1,a0),(d7,d6,d5,d4,d3,d2,d1,d0))
+
     returnA -< dataOut
 
+-- should be able to generalise this into a horizontal expansion somehow with an oporator
+-- ((a2,a1,a0),write,(dataIn1,dataIn2)) -> (out1,out2)
+ram8t2 :: Circuit ((Bit,Bit,Bit),Bit,(Bit,Bit)) (Bit,Bit)
+ram8t2 = proc ((a2,a1,a0),write,(dat1,dat2)) -> do
 
-blend' = (foldr($)[].) . (.map(:)) . zipWith(.) . map(:)
+    out1 <- ram8t1 -< ((a2,a1,a0),write,dat1)
+    out2 <- ram8t1 -< ((a2,a1,a0),write,dat2)
 
+    returnA -< (out1,out2)
+
+-- page 200
+-- ((a3,a2,a1,a0),write,dataIn) -> dataOut
+ram16t1 :: Circuit ((Bit,Bit,Bit,Bit),Bit,Bit) Bit
+ram16t1 = proc ((a3,a2,a1,a0),write,dat) -> do
+
+    (do1,do0) <- decoder1to2 -< (a3,dat)
+    d1        <- ram8t1       -< ((a2,a1,a0),write,do1)
+    d0        <- ram8t1       -< ((a2,a1,a0),write,do0)
+    dataOut   <- selector2to1 -< (a3,(d0,d1))
+
+    returnA -< dataOut
+
+blend'    = (foldr($)[].) . (.map(:)) . zipWith(.) . map(:)
 altList n = take n $ blend'  (repeat Low) (repeat High)
 
 --main :: IO Bit
 main = do
     print $ runCircuit (delay 0) [5,6,7]
     print $ "finished"
-    let x = runCircuit rippleCounter8Bit  $ altList 256
-    mapM_ (putStrLn . show) x
-
